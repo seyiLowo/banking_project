b7e4c3831e231efde39546cd11f05d72e4ebf4ad
Oluwaseyi Oyelowo <seyilowo@gmail.com>
2019-03-18 13:12:37 +0100

    Update sDetails.php

--- sDetails.php
+++ sDetails.php
@@ -132,7 +132,7 @@
                             <nav aria-label="breadcrumb">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="index2.php">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Library</li>
+                                    <li class="breadcrumb-item active" aria-current="page">Edit Account Details</li>
                                 </ol>
                             </nav>
                         </div>
