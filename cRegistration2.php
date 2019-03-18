7c8a7bf84f3b1a2413144810cc94cfe501d3385e
Oluwaseyi Oyelowo <seyilowo@gmail.com>
2019-03-18 13:06:09 +0100

    Update cRegistration2.php

--- cRegistration2.php
+++ cRegistration2.php
@@ -207,7 +207,7 @@
                             <nav aria-label="breadcrumb">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="index2.php">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Library</li>
+                                    <li class="breadcrumb-item active" aria-current="page">Create Account</li>
                                 </ol>
                             </nav>
                         </div>
