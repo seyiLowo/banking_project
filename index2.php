c4434054e0b447a44b468064459e8a80f1fe4e20
Oluwaseyi Oyelowo <seyilowo@gmail.com>
2019-03-18 13:11:29 +0100

    Update index2.php

--- index2.php
+++ index2.php
@@ -124,7 +124,7 @@
                             <nav aria-label="breadcrumb">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Library</li>
+                                    <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
                                 </ol>
                             </nav>
                         </div>
