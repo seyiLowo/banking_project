c55497952c4f51d131dcbce9fe5da7d48d1802ad
Oluwaseyi Oyelowo <seyilowo@gmail.com>
2019-03-18 13:13:56 +0100

    Update sRegistration2.php

--- sRegistration2.php
+++ sRegistration2.php
@@ -206,7 +206,7 @@
                             <nav aria-label="breadcrumb">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="index2.php">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Library</li>
+                                    <li class="breadcrumb-item active" aria-current="page">Create a Staff Account</li>
                                 </ol>
                             </nav>
                         </div>
