8518e0fbe94aa8702f7cea4ff6c7daf1ed6439d1
Oluwaseyi Oyelowo <seyilowo@gmail.com>
2019-03-18 13:07:07 +0100

    Update clients.php

--- clients.php
+++ clients.php
@@ -125,7 +125,7 @@ session_start();
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="index2.php">Home</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Library</li>
+                                <li class="breadcrumb-item active" aria-current="page">Client List</li>
                             </ol>
                         </nav>
                     </div>
