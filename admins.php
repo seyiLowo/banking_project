f841120f15e8541df957b8469f579e8e3dafdea2
Oluwaseyi Oyelowo <seyilowo@gmail.com>
2019-03-18 13:04:32 +0100

    Update admins.php

--- admins.php
+++ admins.php
@@ -121,7 +121,7 @@ session_start();
                             <nav aria-label="breadcrumb">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="index2.php">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Library</li>
+                                    <li class="breadcrumb-item active" aria-current="page">Staff List</li>
                                 </ol>
                             </nav>
                         </div>
